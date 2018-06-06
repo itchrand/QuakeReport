97f063d60379c0c46c6932f2a9581b84c2533829
itchrand <ac_is_nowhere@yahoo.com>
2018-06-06 22:51:21 +0200

    Add preferences.

--- app/src/main/java/it/chrand/quakereport/EarthquakeActivity.kt
+++ app/src/main/java/it/chrand/quakereport/EarthquakeActivity.kt
@@ -12,18 +12,41 @@ import android.content.Loader
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.net.Uri
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.preference.PreferenceManager
+import android.content.SharedPreferences
+
 
 class EarthquakeActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
     val LOG_TAG = EarthquakeActivity::class.java.name
-    private val USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10"
+    private val USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+    //private val USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10"
     lateinit var itemsAdapter: EarthquakeAdapter
 
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<List<Earthquake>> {
-        return EarthquakeLoader(this, USGS_REQUEST_URL)
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default))
+        val orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default))
+
+        val baseUri = Uri.parse(USGS_REQUEST_URL)
+        val uriBuilder = baseUri.buildUpon()
+
+        uriBuilder.appendQueryParameter("format", "geojson")
+        uriBuilder.appendQueryParameter("eventtype", "earthquake")
+        uriBuilder.appendQueryParameter("limit", "10")
+        uriBuilder.appendQueryParameter("minmag", minMagnitude)
+        uriBuilder.appendQueryParameter("orderby", orderBy)
+
+        return EarthquakeLoader(this, uriBuilder.toString())
     }
 
     override fun onLoadFinished(loader: Loader<List<Earthquake>>?, data: List<Earthquake>?) {
@@ -67,6 +90,21 @@ class EarthquakeActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Li
         val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
         return !(networkInfo == null || !networkInfo.isConnected() ||
                 (networkInfo.getType() != ConnectivityManager.TYPE_WIFI &&
-                 networkInfo.getType() != ConnectivityManager.TYPE_MOBILE))
+                        networkInfo.getType() != ConnectivityManager.TYPE_MOBILE))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == R.id.action_settings) {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
